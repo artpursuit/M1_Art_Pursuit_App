26e63f2f730260ff01767a1a7c4f61113a0a534d
lokesh J <98685112+artpursuit@users.noreply.github.com>
2022-02-11 13:43:52 +0530

    Uploded

---
+++ 3_Implementation/inc/main.h
@@ -0,0 +1,20 @@
+#ifndef __ARTPURSUIT_H__
+#define __ARTPURSUIT_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+
+struct artpursuit
+{
+	char name[30];
+	char email[30];
+	char address[100];
+	int phonenumber[10];
+};
+
+struct artpursuit a;
+
+#endif
